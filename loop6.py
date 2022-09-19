dd60f3820d060813b7b82b5133d90564165a8a50
hp <eng.ramy1312@gmail.com>
2022-09-19 19:47:53 +0300

    Add a list of elements to a set and Return a new set of identical items from two sets

--- loop6.py
+++ loop6.py
@@ -3,7 +3,6 @@ sample_list = ["Blue", "Green", "Red"]
 result = sample_set.union(sample_list)
 print(result)
 
-
 set1 = {10, 20, 30, 40, 50}
 set2 = {30, 40, 50, 60, 70}
 result2 = set1.intersection(set2)
